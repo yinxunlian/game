ea0f9e641faf34cd9f2398758c3202597bf55fe3
yinxunlian <yinxunlian@outlook.com>
2018-12-27 15:35:16 +0800

    初始化导入

--- src/main/java/com/game/service/impl/AccountServiceImpl.java
+++ src/main/java/com/game/service/impl/AccountServiceImpl.java
@@ -1,7 +1,6 @@
 package com.game.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.Wrapper;
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.api.Assert;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -23,7 +22,7 @@ import java.util.Date;
  * 账号表 服务实现类
  * </p>
  *
- * @author jobob
+ * @author yinxunlian
  * @since 2018-12-25
  */
 @Service
